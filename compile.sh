5490e4a9a9c5003213b957799e1ee6169aa3c272
Wolf <wolfmoyaers@gmail.com>
2022-11-22 14:31:18 +0100

    new themes

--- compile.sh
+++ compile.sh
@@ -6,4 +6,9 @@ BASEDIR=$(dirname "$0")
  # obsidian 
 sass --no-source-map \
     $BASEDIR/scss/obsidian/main.scss:$BASEDIR/compiled-themes/obsidian/theme.css \
-    $BASEDIR/scss/firefox/styling/config.scss:$BASEDIR/compiled-themes/firefox/chrome/config.css
+    $BASEDIR/scss/firefox/chrome/config.scss:$BASEDIR/compiled-themes/firefox/chrome/config.css \
+    $BASEDIR/scss/firefox/treestyletab/treestyletab.scss:$BASEDIR/compiled-themes/firefox/treestyletab/treestyletab.css
+
+# obsidian for theme
+sass --no-source-map \
+    $BASEDIR/scss/obsidian/main.scss:$BASEDIR/theme.css \
